d23c321e898a6b24674afd71c4bde40226c5daab
valmg11 <tictactina05@gmail.com>
2026-03-24 21:02:16 -0400

    app setup

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { CheckBox } from '@rneui/themed';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
+      <Text>Testing</Text>
       <StatusBar style="auto" />
     </View>
   );
