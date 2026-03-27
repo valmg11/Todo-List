550db04798d8234e7aae05ef0d42b91f09872cd1
valmg11 <tictactina05@gmail.com>
2026-03-26 20:06:57 -0400

    added butons and input box

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, {useState, useCallback} from 'react';
+import { StyleSheet, Text, View, FlatList, Button} from 'react-native';
+import { TextInput } from 'react-native-web';
 // import { CheckBox } from '@rneui/themed';
 
 let data = ["Tsuki", "Mashiro", "Niko", "Yo-Chan","Katsunori"];
 
 export default function App() {
+  let [newMenuItem, setNewMenuItem] = useState("");
+  let [menuItems, setMenuItems] = useState("");
+
+  let addMenuItem = useCallback(() => {
+    console.log("added")
+    setMenuItems([newMenuItem, ...menuItems])
+  }, [newMenuItem, menuItems])
+
+  let [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Awesome Seals</Text>
@@ -14,6 +25,29 @@ export default function App() {
       keyExtractor={(index) => index.toString()}
       />
       <StatusBar style="auto" />
+
+
+      <TextInput 
+      placeholder="Add todo item..."
+      style={{
+          height: 40,
+          padding: 5,
+          marginHorizontal: 8,
+          borderWidth: 1,
+          marginBottom: 40,
+        }}
+      />
+      
+      <Text>Counter {counter}</Text>
+      <Button title="hi" style={[styles.button]}
+        onPress={() => {setCounter(counter +1), console.log("count")}}
+        color="red"></Button>
+
+        <Button title="add" style={[styles.button]}
+        onPress={() => {
+          addMenuItem(), 
+          console.log("addItem")}}
+        color="blue"></Button>
     </View>
   );
 }
@@ -29,5 +63,8 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 25,
     fontFamily: 'sans-serif',
+  },
+  button: {
+    marginBottom: 100,
   },
 });
